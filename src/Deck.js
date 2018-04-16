8fb61898bee1794fa5024d5a5cbf6f84b760be3c
=Isaac <=trevex2000@live.com>
2018-04-16 13:02:01 +0300

    Lesson 24

--- src/Deck.js
+++ src/Deck.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { 
     View, 
     Animated,
-    PanResponder
+    PanResponder, 
+    Dimensions
 } from 'react-native';
 
+const SCREEN_WIDTH = Dimensions.get('window').width;
 class Deck extends Component {
 
     constructor(props) {
@@ -24,13 +26,26 @@ class Deck extends Component {
         this.position = position;
     }
 
+    getCardStyle() {
+        //interpolation 
+        const rotate = this.position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+
+        return {
+            ...this.position.getLayout(),
+            transform: [{ rotate }]
+        };
+    }
+
     renderCards() {
         return this.props.data.map((item, index) => {
             if (index === 0) {
                 return (
                     <Animated.View
                         key={index}
-                        style={this.position.getLayout()}
+                        style={this.getCardStyle()}
                         {...this.panResponder.panHandlers} 
                     >
                         {this.props.renderCard(item)}
